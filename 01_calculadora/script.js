71809b0f83be1e6acd35b2ccfb55f176de24bdee
Miguel Teran <miguel.teranj02@gmail.com>
2023-08-09 18:55:39 -0400

    Comencé con el proyecto de la lista de tareas con vanilla javascript

--- 01_calculadora/script.js
+++ 01_calculadora/script.js
@@ -2,6 +2,12 @@ const d = document;
 
 const $display = d.querySelector('.display');
 const $buttonsContainer = d.querySelector('.buttons');
+const $resetButton = d.querySelector('.reset-button');
+
+let firstValue = '';
+let secondValue = '';
+let operation = null;
+
 const buttonsValues = [
 	'7',
 	'8',
@@ -21,16 +27,67 @@ const buttonsValues = [
 	'+',
 ];
 
-buttonsValues.forEach((val) => {
+function createButton(val) {
 	const button = d.createElement('button');
 	button.textContent = val;
+	button.addEventListener('click', handleButtonClick);
 	$buttonsContainer.appendChild(button);
-	$display.value = '';
+}
 
-	button.addEventListener('click', () => {
-		const regex = /^[0-9]$/;
-		if (regex.test(val)) {
-			console.log(val);
+function handleButtonClick(e) {
+	const val = e.target.textContent;
+	const regex = /^[0-9.]$/;
+
+	if (regex.test(val)) {
+		if (!operation) {
+			firstValue += val;
+		} else {
+			secondValue += val;
 		}
-	});
-});
+		$display.value += val;
+	} else if (val === '=') {
+		performOperation();
+	} else {
+		if (!operation) {
+			operation = val;
+			$display.value += val;
+		}
+	}
+}
+
+function performOperation() {
+	switch (operation) {
+		case '+':
+			$display.value = parseFloat(firstValue) + parseFloat(secondValue);
+			break;
+
+		case '-':
+			$display.value = parseFloat(firstValue) - parseFloat(secondValue);
+			break;
+
+		case '*':
+			$display.value = parseFloat(firstValue) * parseFloat(secondValue);
+			break;
+
+		case '/':
+			$display.value = parseFloat(firstValue) / parseFloat(secondValue);
+			break;
+
+		default:
+			break;
+	}
+
+	firstValue = $display.value;
+	secondValue = '';
+	operation = null;
+}
+
+function resetCalculator() {
+	firstValue = '';
+	secondValue = '';
+	operation = null;
+	$display.value = '';
+}
+
+buttonsValues.forEach(createButton);
+$resetButton.addEventListener('click', resetCalculator);
